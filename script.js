47c88a40edbda1cae56d95318e836837e5cfc6e1
KP <kumash123@gmail.com>
2020-10-09 03:40:26 -0400

    Added event listener for QOL

--- script.js
+++ script.js
@@ -251,3 +251,10 @@ $('#search').on('click', function(){
 });
 loadSearches();
 getCityData('Toronto');
+
+// Execute a function when the user releases a key on the keyboard
+document.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.code === 13) {
+        document.querySelector('#search').click()
+    }
